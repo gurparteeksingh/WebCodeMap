6e42ab11b487f8ae6102e81cc7e5b785de9b12b9
gpteek <gpteek@gmail.com>
2019-06-23 21:44:24 +1000

    easy to edit paths on mapGen.py

--- mapGen.py
+++ mapGen.py
@@ -4,9 +4,13 @@ import sys
 import pygraphviz
 import json
 
+#Paths:
+CodeMapRepoLoaction = "/Users/gs/Documents/WebCodeMap"
+SVFfolderlocation = "/Users/gs/Documents/Capstone/SVF/SVF"
+
 def main():
     G = pygraphviz.AGraph()
-    G.read("/Users/gs/Documents/Capstone/SVF/SVF/callgraph_final.dot")
+    G.read("{}/callgraph_final.dot".format(SVFfolderlocation))
     file = {"nodes" : [], "links" : []}
     for n in G.nodes():
         node = {"id" : n, "user" : "vish", "description" : n.attr['label'], "color" : n.attr['color'], 
@@ -20,7 +24,7 @@ def main():
                 link = {"source" : n, "target" : target}
                 file["links"].append(link)
                 sys.stdout.write("%s,\n" % (json.dumps(file)))      
-    with open('/Users/gs/Documents/WebCodeMap/static/datasets/blocks.json', 'w') as outfile:
+    with open('{}/static/datasets/blocks.json'.format(CodeMapRepoLoaction), 'w') as outfile:
         json.dump(file, outfile)
 if __name__ == "__main__":
     main()
